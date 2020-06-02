6045bdd32ee86878174477e2d27f877391e1d61b
Maximiano <edu.maxi.96@gmail.com>
2020-06-02 14:58:15 -0300

    Separando Componentes

--- src/app.js
+++ src/app.js
@@ -1,40 +1,32 @@
 'use strict'
 
 import React from 'react'
+import Search from './components/search'
+import UserInfo from './components/user-info'
+import Actions from './components/actions'
+import Repos from './components/repos'
 
 const App = () => (
   <div className='app'>
-    <div className='search'>
-      <input type='search' placeholder='Digite o nome do usuário do GitHub' />
-    </div>
-    <div className='user-info'>
-      <img src='https://avatars3.githubusercontent.com/u/17346799?v=4' />
-      <h1>
-        <a href='https://github.com/MaximianoEduardo'>Maximiano Eduardo</a>
-      </h1>
-      <ul className='repos-info'>
-        <li> - Repositorios: 18</li>
-        <li> - Seguidores: 3</li>
-        <li> - Seguindo: 6</li>
-      </ul>
-      <div className='actions'>
-        <button> Ver repositórios </button>
-        <button> Ver favoritos </button>
-      </div>
-      <div>
-        <h2> Repositórios</h2>
-        <ul className='repos'>
-          <li><a href='#'>Nome do Repositorio</a></li>
-        </ul>
-      </div>
-      <div>
-        <h2>Favoritos</h2>
-        <ul className='starred'>
-          <li><a href=''>Nome do Repositorio</a></li>
-        </ul>
-      </div>
-    </div>
+    <Search />
+    <UserInfo />
+    <Actions />
+    <Repos
+      className='repos'
+      title='Repositórios'
+      repos={[{
+        name: 'Nome do repositório',
+        link: '#'
+      }]}
+    />
+    <Repos
+      className='repos'
+      title='Favoritos'
+      repos={[{
+        name: 'Nome do repositório',
+        link: '#'
+      }]}
+    />
   </div>
 )
-
 export default App
---
+++ src/components/user-info.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import React from 'react'
+
+const UserInfo = () => (
+  <div className='user-info'>
+    <img src='https://avatars3.githubusercontent.com/u/17346799?v=4' />
+    <h1>
+      <a href='https://github.com/MaximianoEduardo'>Maximiano Eduardo</a>
+    </h1>
+    <ul className='repos-info'>
+      <li> - Repositorios: 18</li>
+      <li> - Seguidores: 3</li>
+      <li> - Seguindo: 6</li>
+    </ul>
+  </div>
+)
+
+export default UserInfo
